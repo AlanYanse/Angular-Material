71fff1486b6225053c4441056c86880a3f3272b1
Alan Yanse <yansealan@gmail.com>
2022-08-30 22:55:27 -0300

    agregando funcionalidad al login parte 1

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private _snackBar: MatSnackBar) {
+      this.form = this.formBuilder.group({
+      usuario: ["", Validators.required],
+      password: ["", Validators.required]
+    });
+   }
 
   ngOnInit(): void {
+    
+  }
+
+  ingresar(){
+    
+    // Capturando valores que vienen del form
+    const usuario: string = this.form.value.usuario;
+    const password: string = this.form.value.password;
+
+    this.validacion(usuario , password)
+  }
+
+  validacion(usuario: string, password: string) {
+
+    let fakeUser: string = "cosme";
+    let fakePass: string = "fulanito";
+
+    if(fakeUser === usuario && fakePass === password){
+      // Al dashboard
+    }else{
+      // mensaje de error
+      this.error();
+    }
+
+  }
+
+  error(){
+
+    this._snackBar.open("algunos de los datos ingresados no son válidos", "", {
+      duration: 4000,
+      horizontalPosition: "center",
+      verticalPosition: "top"
+    })
+
   }
 
 }
